2dd7eb011451ebc7d3b97020ca1611a33a722ac6
wcnjing <s10242078@connect.np.edu.sg>
2023-01-23 15:38:05 +0800

    .

--- myscript.js
+++ myscript.js
@@ -6,3 +6,18 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+// animation
+const observer = new IntersectionObserver((entries)=> {
+  entries.forEach((entry)=> {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+
+  });
+});
+  const hiddenElements = document.querySelectorAll('.ani');
+  hiddenElements.forEach((e1) => observer.observe(e1));
+
