1f8d9430c3e52d87e25ecc6453e4cbf31cae63c0
wcnjing <s10242078@connect.np.edu.sg>
2023-02-06 12:03:14 +0800

    .

--- myscript.js
+++ myscript.js
@@ -23,7 +23,7 @@ const observer = new IntersectionObserver((entries)=> {
 
 //Webb Telescope Moving Slides
 var slide = 0;
-var slideshowImages = new Array("/images/WebbSlideShowA.jpg", "/images/WebbSlideshowB.jpg", "/images/WebbSlideshowC.jpg")
+var slideshowImages = new Array("images/WebbSlideShowA.jpg", "images/WebbSlideshowB.jpg", "images/WebbSlideshowC.jpg")
 
 var description = new Array(" The Phantom Galaxy<Br>The Phantom Galaxy is a spiral galaxy where tendrils of darkness<br> stretch out from its brilliant blue core. While the Hubble photographed<br> the Galaxy previously, the Webb Telescope has uncovered the strains<br> of heat emitting gas and dust.",
 "The Southern Ring Nebula<Br> The Southen Ring Nebula was orignially thought to possess<br> only a White Dwarf Star Until a closer inspection provided <br> by the Webb Telescope revealed not one, but three <br> stars within the nebula, one of which is theorised to<br> be emitting gas that is creating that 'halo' like veil", 
